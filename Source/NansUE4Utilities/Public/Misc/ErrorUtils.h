ccad3e4ff9e0272e0e8871c4ea9e97185970e546
Nans Pellicari <nans.pellicari@gmail.com>
2020-12-17 15:17:38 +0100

    improve:
    * EDITOR_WARN can use other object than this
    * Editor error and warns print log if not WITH_EDITOR

--- Source/NansUE4Utilities/Public/Misc/ErrorUtils.h
+++ Source/NansUE4Utilities/Public/Misc/ErrorUtils.h
@@ -1,66 +1,109 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Engine/Engine.h"
-#include "EngineGlobals.h"
 #include "Logging/MessageLog.h"
 #include "Runtime/CoreUObject/Public/Misc/UObjectToken.h"
 #include "UObject/NoExportTypes.h"
+#include "Engine/Engine.h"
+#include "EngineGlobals.h"
 
-#if WITH_EDITOR
+
 #define EDITOR_ERROR_1(MsgChannel, Text) UNErrorUtils::ThrowErrorToEditor(MsgChannel, Text, this, __LINE__);
-#define EDITOR_ERROR_2(MsgChannel, Text, Object) UNErrorUtils::ThrowErrorToEditor(MsgChannel, Text, Object, __LINE__);
+#define EDITOR_ERROR_2(MsgChannel, Text, Object) UNErrorUtils::ThrowErrorToEditor(MsgChannel, Text, Cast<UObject>(Object), __LINE__);
 #define EDITOR_ERROR_0()
+#define EDITOR_WARN_1(MsgChannel, Text) UNErrorUtils::ThrowWarningToEditor(MsgChannel, Text, this, __LINE__);
+#define EDITOR_WARN_2(MsgChannel, Text, Object) UNErrorUtils::ThrowWarningToEditor(MsgChannel, Text, Cast<UObject>(Object), __LINE__);
+#define EDITOR_WARN_0()
 
 // thanks to https://github.com/jason-deng/C99FunctionOverload
 #define FUNC_CHOOSER(_f1, _f2, _f3, _f4, ...) _f4
 #define FUNC_RECOMPOSER(argsWithParentheses) FUNC_CHOOSER argsWithParentheses
+
 #define CHOOSE_FROM_ARG_COUNT(...) FUNC_RECOMPOSER((__VA_ARGS__, EDITOR_ERROR_2, EDITOR_ERROR_1, ))
 #define NO_ARG_EXPANDER() , , EDITOR_ERROR_0
 #define MACRO_CHOOSER(...) CHOOSE_FROM_ARG_COUNT(NO_ARG_EXPANDER __VA_ARGS__())
 
+#define CHOOSE_FROM_ARG_COUNT_WARN(...) FUNC_RECOMPOSER((__VA_ARGS__, EDITOR_WARN_2, EDITOR_WARN_1, ))
+#define NO_ARG_EXPANDER_WARN() , , EDITOR_WARN_0
+#define MACRO_CHOOSER_WARN(...) CHOOSE_FROM_ARG_COUNT_WARN(NO_ARG_EXPANDER_WARN __VA_ARGS__())
+
 #define EDITOR_ERROR(...) MACRO_CHOOSER(__VA_ARGS__)(__VA_ARGS__)
-#define EDITOR_WARN(...) UNErrorUtils::ThrowWarningToEditor(__VA_ARGS__, this, __LINE__);
+#define EDITOR_WARN(...) MACRO_CHOOSER_WARN(__VA_ARGS__)(__VA_ARGS__)
+
+#if WITH_EDITOR
 #define SCREEN_LOG(duration, color, format, ...) \
 	if (GEngine) GEngine->AddOnScreenDebugMessage(-1, duration, color, FString::Printf(TEXT(format), ##__VA_ARGS__), false)
 #else
-#define EDITOR_ERROR(...)
-#define EDITOR_WARN(...)
 #define SCREEN_LOG(duration, color, format, ...)
 #endif
 
-#if WITH_EDITOR
+
 class UNErrorUtils
 {
 public:
 	static void ThrowErrorToEditor(const FName& LogMsgChannel, const FText& Text, const UObject* Object,
 		const int32& Line = -1)
 	{
-		auto Message = FMessageLog(LogMsgChannel).Error();
-		Message->AddToken(FTextToken::Create(Text))->AddToken(FUObjectToken::Create((UObject*)Object));
-		if (Line >= 0)
+		bool bValidObject = IsValid(Object) && !Object->HasAnyFlags(
+								EObjectFlags::RF_BeginDestroyed | EObjectFlags::RF_FinishDestroyed
+							)
+							&& IsValid(Object->GetOuter()) && IsValid(Object->GetWorld());
+#if WITH_EDITOR
+		if (bValidObject)
 		{
-			Message->AddToken(
-				FTextToken::Create(FText::Format(NSLOCTEXT("Error", "LineNumber", "- line {0}"), FText::AsNumber(Line)))
-			);
+			auto Message = FMessageLog(LogMsgChannel).Error();
+			Message->AddToken(FTextToken::Create(Text))->AddToken(FUObjectToken::Create((UObject*)Object));
+			if (Line >= 0)
+			{
+				Message->AddToken(
+					FTextToken::Create(
+						FText::Format(NSLOCTEXT("Error", "LineNumber", "- line {0}"), FText::AsNumber(Line))
+					)
+				);
+			}
 		}
+#endif
 
-		UE_LOG(LogTemp, Error, TEXT("%s (%s) - line %d"), *Text.ToString(), *Object->GetName(), Line);
+		UE_LOG(
+			LogTemp,
+			Error,
+			TEXT("%s (%s) - line %d"),
+			*Text.ToString(),
+			bValidObject ? *Object->GetName() : TEXT("Unknown"),
+			Line
+		);
 	}
 
 	static void ThrowWarningToEditor(const FName& LogChannel, const FText& Text, const UObject* Object,
 		const int32& Line = -1)
 	{
-		auto Message = FMessageLog(LogChannel).Warning();
-		Message->AddToken(FTextToken::Create(Text))->AddToken(FUObjectToken::Create((UObject*)Object));
-		if (Line >= 0)
+		bool bValidObject = IsValid(Object) && !Object->HasAnyFlags(
+								EObjectFlags::RF_BeginDestroyed | EObjectFlags::RF_FinishDestroyed
+							)
+							&& IsValid(Object->GetOuter()) && IsValid(Object->GetWorld());
+#if WITH_EDITOR
+		if (bValidObject)
 		{
-			Message->AddToken(
-				FTextToken::Create(FText::Format(NSLOCTEXT("Error", "LineNumber", "- line {0}"), FText::AsNumber(Line)))
-			);
+			auto Message = FMessageLog(LogChannel).Warning();
+			Message->AddToken(FTextToken::Create(Text))->AddToken(FUObjectToken::Create((UObject*)Object));
+			if (Line >= 0)
+			{
+				Message->AddToken(
+					FTextToken::Create(
+						FText::Format(NSLOCTEXT("Error", "LineNumber", "- line {0}"), FText::AsNumber(Line))
+					)
+				);
+			}
 		}
+#endif
 
-		UE_LOG(LogTemp, Warning, TEXT("%s (%s) - line %d"), *Text.ToString(), *Object->GetName(), Line);
+		UE_LOG(
+			LogTemp,
+			Warning,
+			TEXT("%s (%s) - line %d"),
+			*Text.ToString(),
+			bValidObject ? *Object->GetName() : TEXT("Unknown"),
+			Line
+		);
 	}
 };
-#endif
